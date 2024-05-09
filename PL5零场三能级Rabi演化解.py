a131ee4f4da1c20e75e2934951aa6e00afc3ea0e
Victor Teng <xassam@mail.ustc.edu.cn>
2024-05-09 10:17:10 +0800

    加入了含时Rabi  (Pulse CW)

--- PL5零场三能级Rabi演化解.py
+++ PL5零场三能级Rabi演化解.py
@@ -6,12 +6,12 @@ from qutip import *
 import mplcursors
 
 #参数设定
-delta = 2 * np.pi * 0.5 * 29.0800 * 10 ** (-3) # 劈裂30MHz
+Delta = 2 * np.pi * 0.5 * 29.0800 * 10 ** (-3) # 劈裂30MHz
 Rabi_R = 2 * np.pi * 0.5 * 12.6522160260718 * 10 ** (-3) # 右峰操控强度10MHz
 Rabi_L = 2 * np.pi * 0.5 * 3.40543836161306 * 10 ** (-3) # 左峰操控强度3MHz
 
 H_MW = Qobj([[0, Rabi_R, 0],[Rabi_R, 0, 1j * Rabi_L],[0, -1j * Rabi_L, 0]]) #基为{+,0,-}
-H_Level = Qobj([[delta,0,0],[0,0,0],[0,0,0]])
+H_Level = Qobj([[Delta,0,0],[0,0,0],[0,0,0]])
 H_T =  H_MW + H_Level# 在左峰旋转坐标系下的总哈密顿量 = 微波哈密顿量 + 能级哈密顿量
 
 # 定义初始状态（假设实验操控+1和0能级）
@@ -64,4 +64,30 @@ cursor = mplcursors.cursor(hover=True)
 cursor.connect("add", lambda sel: sel.annotation.set_text(f'({sel.target[0]}, {sel.target[1]})'))
 plt.show()
 print(freq[np.argmax(Spectrum)])
+
+# plot含时Rabi (Pulse-CW)
+t_Pi = 0.5 * abs(1/freq[np.argmax(Spectrum)])
+Popu_0_f = []
+
+delta_list = np.arange(-10,10,0.05)
+delta_list.reshape(-1,1)
+for delta in delta_list:
+    H_Level = Qobj([[Delta-delta,0,0],[0,0,0],[0,0,-delta]])
+    H_T =  H_MW + H_Level
+    O_Evolve = (-1j * t_Pi * H_T).expm()
+    psi_f = O_Evolve * psi0
+    Coeff_0_f = psi_f[1]
+    Popu_0_f.append(abs(Coeff_0_f[0,0]) ** 2)
+
+plt.figure(figsize=(8, 5))
+plt.plot(delta_list, Popu_0_f, label=f"0")
+plt.xlabel('Delta')
+plt.ylabel('Population')
+plt.legend()
+cursor = mplcursors.cursor(hover=True)
+cursor.connect("add", lambda sel: sel.annotation.set_text(f'({sel.target[0]}, {sel.target[1]})'))
+plt.show()
+
+
+
 
