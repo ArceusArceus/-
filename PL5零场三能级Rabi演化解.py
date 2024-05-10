06b9fb4e7b612e7281403a21771268142c5f16b4
Victor Teng <xassam@mail.ustc.edu.cn>
2024-05-10 16:07:40 +0800

    增加了对Pulse CW的左峰再做Rabi

--- PL5零场三能级Rabi演化解.py
+++ PL5零场三能级Rabi演化解.py
@@ -6,9 +6,9 @@ from qutip import *
 import mplcursors
 
 #参数设定
-Delta = 2 * np.pi * 0.5 * 29.0800 * 10 ** (-3) # 劈裂30MHz
-Rabi_R = 2 * np.pi * 0.5 * 12.6522160260718 * 10 ** (-3) # 右峰操控强度10MHz
-Rabi_L = 2 * np.pi * 0.5 * 3.40543836161306 * 10 ** (-3) # 左峰操控强度3MHz
+Delta = 2 * np.pi * 0.5 *  48.9900 * 10 ** (-3) # 劈裂30MHz
+Rabi_R = 2 * np.pi * 0.5 * 12.2754704975671 / np.sqrt(2)  * 10 ** (-3) # 右峰操控强度10MHz
+Rabi_L = 2 * np.pi * 0.5 * 2.19103192747710 / np.sqrt(2)  * 10 ** (-3) # 左峰操控强度3MHz
 
 H_MW = Qobj([[0, Rabi_R, 0],[Rabi_R, 0, 1j * Rabi_L],[0, -1j * Rabi_L, 0]]) #基为{+,0,-}
 H_Level = Qobj([[Delta,0,0],[0,0,0],[0,0,0]])
@@ -18,7 +18,7 @@ H_T =  H_MW + H_Level# 在左峰旋转坐标系下的总哈密顿量 = 微波哈
 psi0 = Qobj([[0],[1],[0]])   # 初态为0
 
 # 定义时间点
-tlist = np.linspace(0, 1000, 10000) 
+tlist = np.linspace(0, 10000, 10000) 
 
 # 演化
 Popu_0 = []
@@ -36,11 +36,12 @@ for n in range(len(tlist)):
 
     
 
-# 绘制0能级占据概率随时间的变化
+# 绘制0,+,-能级占据概率随时间的变化
+N = int(len(tlist)/10)
 plt.figure(figsize=(8, 5))
-plt.plot(tlist, Popu_0, label=f"0")
-plt.plot(tlist, Popu_p, label=f"+")
-plt.plot(tlist, Popu_m, label=f"-")
+plt.plot(tlist[:N], Popu_0[:N], label=f"0")
+plt.plot(tlist[:N], Popu_p[:N], label=f"+")
+plt.plot(tlist[:N], Popu_m[:N], label=f"-")
 plt.xlabel('Time')
 plt.ylabel('Population')
 plt.legend()
@@ -51,11 +52,12 @@ plt.show()
 # 用cos拟合0能级曲线，观察拟合出的Rabi频率与实际设定的左峰频率的差别
 Popu_0 = Popu_0 - np.mean(Popu_0)
 Spectrum = np.fft.fft(Popu_0)
+Spectrum = np.abs(Spectrum)
 n = len(tlist)  # 数据点数
 T = tlist[1] - tlist[0]  # 采样间隔
 freq = np.fft.fftfreq(n, T)# 频率轴
 plt.figure()
-plt.plot(freq, np.abs(Spectrum))
+plt.plot(freq, Spectrum)
 plt.title('FFT of the signal')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Amplitude')
@@ -69,9 +71,10 @@ print(freq[np.argmax(Spectrum)])
 t_Pi = 0.5 * abs(1/freq[np.argmax(Spectrum)])
 Popu_0_f = []
 
-delta_list = np.arange(-10,10,0.05)
+delta_list = np.arange(-50,100,0.01) #干脆把右峰的Pulse CW也plot出来
 delta_list.reshape(-1,1)
-for delta in delta_list:
+delta_list_H = 2 * np.pi * 0.5 *  delta_list * 10 ** (-3)
+for delta in delta_list_H:
     H_Level = Qobj([[Delta-delta,0,0],[0,0,0],[0,0,-delta]])
     H_T =  H_MW + H_Level
     O_Evolve = (-1j * t_Pi * H_T).expm()
@@ -81,12 +84,51 @@ for delta in delta_list:
 
 plt.figure(figsize=(8, 5))
 plt.plot(delta_list, Popu_0_f, label=f"0")
-plt.xlabel('Delta')
+plt.xlabel('delta from Left Peak Resonance, positive = higher freq than Left Peak')
 plt.ylabel('Population')
 plt.legend()
 cursor = mplcursors.cursor(hover=True)
 cursor.connect("add", lambda sel: sel.annotation.set_text(f'({sel.target[0]}, {sel.target[1]})'))
 plt.show()
+
+# 换到模拟的含时Rabi的最低点，注意此时频率不在左峰，比左峰略高
+N = int(70/0.01)
+Popu_0_f = Popu_0_f[:N] #截取左峰，不要右峰
+delta = delta_list_H[np.argmin(Popu_0_f)]
+H_Level = Qobj([[Delta-delta,0,0],[0,0,0],[0,0,-delta]])
+H_T =  H_MW + H_Level
+
+Popu_0 = []
+Popu_p = []
+Popu_m = []
+for n in range(len(tlist)):
+    O_Evolve = (-1j * tlist[n] * H_T).expm()
+    psi_t = O_Evolve * psi0
+    Coeff_0 = psi_t[1]
+    Coeff_p = psi_t[0]
+    Coeff_m = psi_t[2]
+    Popu_0.append(abs(Coeff_0[0,0]) ** 2)
+    Popu_p.append(abs(Coeff_p[0,0]) ** 2)
+    Popu_m.append(abs(Coeff_m[0,0]) ** 2)
+
+N = int(len(tlist)/10)
+plt.figure(figsize=(8, 5))
+plt.plot(tlist[:N], Popu_0[:N], label=f"0")
+plt.plot(tlist[:N], Popu_p[:N], label=f"+")
+plt.plot(tlist[:N], Popu_m[:N], label=f"-")
+plt.xlabel('Time')
+plt.ylabel('Population')
+plt.legend()
+cursor = mplcursors.cursor(hover=True)
+cursor.connect("add", lambda sel: sel.annotation.set_text(f'({sel.target[0]}, {sel.target[1]})'))
+plt.show()
+Popu_0 = Popu_0 - np.mean(Popu_0)
+Spectrum = np.fft.fft(Popu_0)
+Spectrum = np.abs(Spectrum)
+n = len(tlist)  # 数据点数
+T = tlist[1] - tlist[0]  # 采样间隔
+freq = np.fft.fftfreq(n, T)# 频率轴
+print(freq[np.argmax(Spectrum)])
 
 
 
